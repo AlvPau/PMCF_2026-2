263e87132c3fd520eef197492f6af2597c0f72f3
AlvPau <125477358+AlvPau@users.noreply.github.com>
2026-05-01 19:52:02 -0600

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -212,7 +212,10 @@ def render_inciso_f(serie_rend):
 def main():
     st.set_page_config(page_title="Proyecto MCF", layout="wide")
     st.title("Proyecto I – Métodos Cuantitativos en Finanzas")
-    st.markdown("Integrantes: Paulina Álvarez, Vanessa Escobar, Vanessa Jiménez, Elias Santana")
+    st.markdown("1. Álvarez Peralta Paulina Alejandra 
+  2. Escobar García Vanessa
+  3. Jiménez Chávez Vanessa Guadalupe
+  4. Santana Mendoza Elias")
     precios = render_inciso_a()
     serie_rend = render_inciso_b(precios)
     render_inciso_c(serie_rend)
